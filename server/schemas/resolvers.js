464f47702b556fe12ed44f45db7a677eb64f6a53
ryanweiler92 <ryanweiler92@gmail.com>
2022-04-12 10:35:07 -0400

    users query & addUser mutation

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -3,6 +3,19 @@ const { AuthenticationError } = require('apollo-server-express');
 const { signToken } = require('../utils/auth');
 
 const resolvers = {
+    Query: {
+        users: async () => {
+            return await User.find({})
+        }
+    },
+    Mutation: {
+        addUser: async (parent, args) => {
+            const user = await User.create(args);
+            
+          
+            return { user };
+        }
+    }
 
 }
 
--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -5,11 +5,29 @@ type User {
     _id: ID
     username: String
     email: String
+    bookCount: Int
+    savedBooks: [Book]
+}
+
+type Book {
+    bookId: ID
+    authors: String
+    description: String
+    title: String
 }
 
 type Query {
     users: [User]
 }
+
+type Mutation {
+    addUser(username: String!, email: String!, password: String!): User
+}
+
+type Auth {
+    token: ID!
+    user: User
+  }
 `;
 
 module.exports = typeDefs
