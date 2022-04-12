540902aa251b3d9b9e4dc5db4019319b27c769e4
ryanweiler92 <ryanweiler92@gmail.com>
2022-04-12 08:35:31 -0400

    add schemas & auth

--- server/server.js
+++ server/server.js
@@ -2,8 +2,8 @@ const express = require('express');
 const path = require('path');
 const db = require('./config/connection');
 const { ApolloServer } = require('apollo-server-express');
-//not created yet
-// const { typeDefs, resolvers } = require('./schemas');
+const { authMiddleware } = require('./utils/auth');
+const { typeDefs, resolvers } = require('./schemas');
 
 //might not need this?
 const routes = require('./routes');
@@ -11,14 +11,31 @@ const routes = require('./routes');
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+const startServer = async () => {
+  
+  const server = new ApolloServer({ 
+    typeDefs, 
+    resolvers, 
+    context: authMiddleware 
+  });
+
+    await server.start();
+
+    server.applyMiddleware({ app });
+  
+    console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+  };
+  
+startServer();
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// if we're in production, serve client/build as static assets
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
+//might need to delete
 app.use(routes);
 
 db.once('open', () => {
