55d3b5619a929b4ea079e308fad03249525a8153
ryanweiler92 <ryanweiler92@gmail.com>
2022-04-14 10:44:37 -0400

    use tokens on login/adduser mutations client side

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -3,34 +3,24 @@ import { gql } from '@apollo/client';
 export const LOGIN_USER = gql`
 mutation login($email: String!, $password: String!) {
     login(email: $email, password: $password) {
-      
-        user {
-            _id
-            username
-        }
-    }
-}`
-//put token above user when working
-
-
-// export const LOGIN_USER = gql`
-// mutation login($email: String!, $password: String!) {
-//     login(email: $email, password: $password) {
-//       _id
-//       username
-//       email
-//     }
-//   }`
-
-
-export const ADD_USER = gql`
-mutation addUser($username: String!, $email: String!, $password: String!) {
-    addUser(username: $username, email: $email, password: $password) {
+        token
         user {
             _id
             username
         }
     }
 }
-`
+`;
+
+export const ADD_USER = gql`
+mutation addUser($username: String!, $email: String!, $password: String!) {
+    addUser(username: $username, email: $email, password: $password) {
+        token
+        user {
+            _id
+            username
+        }
+    }
+}
+`;
 
