82d8c227cf57b6dc11c42062886ea2be9f9430c7
ryanweiler92 <ryanweiler92@gmail.com>
2022-04-17 10:36:20 -0400

    display books on savedBooks.js with GET_ME query

--- client/src/pages/SavedBooks.js
+++ client/src/pages/SavedBooks.js
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from 'react';
 import { Jumbotron, Container, CardColumns, Card, Button } from 'react-bootstrap';
-import { getMe, deleteBook } from '../utils/API';
 import Auth from '../utils/auth';
 import { removeBookId } from '../utils/localStorage';
 import { useQuery, useMutation } from '@apollo/client';
@@ -8,41 +7,12 @@ import { GET_ME } from '../utils/queries'
 import { REMOVE_BOOK } from '../utils/mutations'
 
 const SavedBooks = () => {
-  // const [userData, setUserData] = useState({});
 
-  const { data: userData } = useQuery(GET_ME)
+  const { loading, error, data } = useQuery(GET_ME)
 
-  const user = userData?.me.savedBooks || {};
-
-  console.log(user)
+  const user = data?.me || {};
 
   const [removeBook] = useMutation(REMOVE_BOOK)
-  
-  
-  // useEffect(() => {
-  //   const getUserData = async () => {
-  //     try {
-  //       const token = Auth.loggedIn() ? Auth.getToken() : null;
-
-  //       if (!token) {
-  //         return false;
-  //       }
-
-  //       const response = await getMe(token);
-
-  //       if (!response.ok) {
-  //         throw new Error('something went wrong!');
-  //       }
-
-  //       const user = await response.json();
-  //       setUserData(user);
-  //     } catch (err) {
-  //       console.error(err);
-  //     }
-  //   };
-
-  //   getUserData();
-  // }, [userDataLength]);
 
   // create function that accepts the book's mongo _id value as param and deletes the book from the database
   const handleDeleteBook = async (bookId) => {
@@ -69,9 +39,9 @@ const SavedBooks = () => {
   };
 
   // if data isn't here yet, say so
-  // if (!userDataLength) {
-  //   return <h2>LOADING...</h2>;
-  // }
+  if (loading) {
+    return <h2>LOADING...</h2>;
+  }
 
   return (
     <>
@@ -82,12 +52,12 @@ const SavedBooks = () => {
       </Jumbotron>
       <Container>
         <h2>
-          {user.length
-            ? `Viewing ${user.length} saved ${user.length === 1 ? 'book' : 'books'}:`
+          {user.savedBooks?.length
+            ? `Viewing ${user.savedBooks?.length} saved ${user.savedBooks?.length === 1 ? 'book' : 'books'}:`
             : 'You have no saved books!'}
         </h2>
         <CardColumns>
-          {user.map((book) => {
+          {user.savedBooks?.map((book) => {
             return (
               <Card key={book.bookId} border='dark'>
                 {book.image ? <Card.Img src={book.image} alt={`The cover for ${book.title}`} variant='top' /> : null}
